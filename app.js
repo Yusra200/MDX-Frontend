70d0eb23fac9e320170b6dcdc6f76431ea7e7ccd
Yuluvweb <yusraibrahim@icloud.com>
2025-10-19 13:08:31 +0100

    message

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 //import function from global 
 const { createApp } = Vue;
 
@@ -7,32 +8,78 @@ const app = createApp({
     //data returns all variables that i have put
     return {
       site: 'Vue.js School Lessons',
-      lessons: [
-        { id: 10, title: 'Maths', description: 'Math Lessons', price: '10' },
-        { id: 11, title: 'English', description: 'English Lessons', price: '11' },
-        { id: 12, title: 'Science', description: 'Science Lessons', price: '12' },
-        { id: 13, title: 'History', description: 'History Lessons', price: '13' },
-        { id: 14, title: 'Computing', description: 'Computing Lessons', price: '14' },
-        { id: 15, title: 'Business', description: 'Business Lessons', price: '15' },
-        { id: 16, title: 'Art', description: 'Art Lessons', price: '16' },
-        { id: 17, title: 'Geography', description: 'Geography Lessons', price: '17' },
-        { id: 18, title: 'Music', description: 'Music Lessons', price: '18' },
-        { id: 19, title: 'Drama', description: 'Drama Lessons', price: '19' }
+      lessons: [ //presenting all the lessons
+        { id: 10, title: 'Maths', availability: 5, location: 'London', price: 10 },
+        { id: 11, title: 'English', availability: 5, location: 'London', price: 11 },
+        { id: 12, title: 'Science', availability: 5, location: 'London', price: 12 },
+        { id: 13, title: 'History', availability: 5, location: 'London', price: 13 },
+        { id: 14, title: 'Computing', availability: 5, location: 'London', price: 14 },
+        { id: 15, title: 'Business', availability: 5, location: 'London', price: 15 },
+        { id: 16, title: 'Art', availability: 5, location: 'London', price: 16 },
+        { id: 17, title: 'Geography', availability: 5, location: 'London', price: 17 },
+        { id: 18, title: 'Music', availability: 5, location: 'London', price: 18 },
+        { id: 19, title: 'Drama', availability: 5, location: 'London', price: 19 }
       ],
       //array stores any lessons that is added to cart
-      cart: []
+      cart: [],
+
+    //show lessons, if not then take to checkout page
+      showLessons: true,
+
+    methods: {
+      showCheckout(){
+        this.showLessons = this.showLessons ? false: true;
+      }
+    }
     };
   },
+
+  computed: {
+    cartItemAmount() {
+      return this.cart.length || ""; //counting length
+    }
+  },
+
   //instead of function option api uses the name
   //this method will run when addtocart button is clicked
   methods: {
-    addToCart() {
+    addToCart(lesson) {
+      if (lesson.availability <= 0) return;
+      lesson.availability--;
       this.cart.push(lesson.id); //pushing new id
-
-      // console.log('Cart:', this.cart);
     }
   }
 });
-
 //connecting to vue and html
 app.mount('#app');
+//window.app = app; //THIS JUST TO CHECK CONSOLE IF WORKING REMOVE AFTER
+
+/* const { createApp } = Vue;
+
+const app = createApp({
+data() {
+return {
+  site: 'Vue.js School Lessons',
+  count: 0,
+  lesson: {
+    title: 'Maths',
+    description: 'Math Lessons',
+    price: 10,
+    id: 10
+    //img
+    },
+    cart: []
+  };
+}
+});
+
+app.mount('#app');
+
+//instead of function option api uses the name
+methods: {
+  addToCart () 
+  {
+    this.cart.push(this.product.id);
+  }
+}
+*/
