cc6d6b293011ff9795ad1de902c55b27efb08e9c
Yuluvweb <yusraibrahim@icloud.com>
2025-10-22 15:53:35 +0100

    message

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-
-//import function from global 
 const { createApp } = Vue;
 
-//create new vue instance
 const app = createApp({
   data() {
-    //data returns all variables that i have put
     return {
-      site: 'Vue.js School Lessons',
-      lessons: [ //presenting all the lessons
+      site: 'School Lessons',
+      //presenting all the lessons
+      lessons: [
         { id: 10, title: 'Maths', availability: 5, location: 'London', price: 10 },
         { id: 11, title: 'English', availability: 5, location: 'London', price: 11 },
         { id: 12, title: 'Science', availability: 5, location: 'London', price: 12 },
@@ -22,64 +19,13 @@ const app = createApp({
       ],
       //array stores any lessons that is added to cart
       cart: [],
-
-    //show lessons, if not then take to checkout page
-      showLessons: true,
-
-    methods: {
-      showCheckout(){
-        this.showLessons = this.showLessons ? false: true;
-      }
-    }
+    
+      //user information for checkout
+      firstName: '',
+      lastName: '',
+      phoneNumber: ''
     };
-  },
-
-  computed: {
-    cartItemAmount() {
-      return this.cart.length || ""; //counting length
-    }
-  },
-
-  //instead of function option api uses the name
-  //this method will run when addtocart button is clicked
-  methods: {
-    addToCart(lesson) {
-      if (lesson.availability <= 0) return;
-      lesson.availability--;
-      this.cart.push(lesson.id); //pushing new id
-    }
   }
 });
-//connecting to vue and html
+//vue connect js to html
 app.mount('#app');
-//window.app = app; //THIS JUST TO CHECK CONSOLE IF WORKING REMOVE AFTER
-
-/* const { createApp } = Vue;
-
-const app = createApp({
-data() {
-return {
-  site: 'Vue.js School Lessons',
-  count: 0,
-  lesson: {
-    title: 'Maths',
-    description: 'Math Lessons',
-    price: 10,
-    id: 10
-    //img
-    },
-    cart: []
-  };
-}
-});
-
-app.mount('#app');
-
-//instead of function option api uses the name
-methods: {
-  addToCart () 
-  {
-    this.cart.push(this.product.id);
-  }
-}
-*/
